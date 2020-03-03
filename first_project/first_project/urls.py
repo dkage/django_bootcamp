282eeff6e5ccaa358e746f0ac4b5717eab632675
Danilo Cunha <danilokage@gmail.com>
2020-03-03 11:21:51 -0300

    [Django first_proj] Fix first app route and fix route for index

--- first_project/first_project/urls.py
+++ first_project/first_project/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+# App import
 from first_app import views
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('index/', views.index, name="index")
+    path('', views.index, name="index"),
+    path('admin/', admin.site.urls)
 ]
