44324d8084bc31b472f2d8a74d638daa5abeaacd
Danilo Cunha <danilokage@gmail.com>
2020-03-05 18:03:39 -0300

    [Django First] Following along lesson for Django Templates

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -4,4 +4,5 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World")
+    test_dict = {'insert_var': 'Hello World Django Template Language'}
+    return render(request, 'first_app/index.html', context=test_dict)
