ee36f27fe39bae065f9fa5d4d6fee7b7e0b4b516
Danilo Cunha <danilokage@gmail.com>
2020-03-07 23:55:52 -0300

    [Django First] Set env for faker call

---
+++ first_project/populate_script.py
@@ -0,0 +1,9 @@
+import set_env_var
+import django
+import random
+from first_app.models import *
+from faker import Faker
+
+
+django.setup()
+
---
+++ first_project/set_env_var.py
@@ -0,0 +1,5 @@
+import os
+
+# Add system wide env var for this runtime
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_project.settings')
+
