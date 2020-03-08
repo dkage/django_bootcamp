a9d61a1c141b0610416e3eb8835e94a5762213ae
Danilo Cunha <danilokage@gmail.com>
2020-03-08 15:21:13 -0300

    [Django First] Add populate script with faker get_or_create functions

--- first_project/populate_script.py
+++ first_project/populate_script.py
@@ -1,9 +1,38 @@
-import set_env_var
+import os
 import django
 import random
-from first_app.models import *
 from faker import Faker
 
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'first_project.settings')
 
 django.setup()
 
+from first_app.models import Topic, Webpage, AccessRecord
+
+fake_gen = Faker()
+topics = ['Search', 'Social', 'Marketplace', 'News', 'Games']
+
+
+def add_topic():
+    t = Topic.objects.get_or_create(top_name=random.choice(topics))[0]
+    t.save()
+    return t
+
+
+def populate(N=5):
+
+    for entry in range(N):
+
+        # Get topic
+        top = add_topic()
+
+        # Create fake data
+        fake_url = fake_gen.url()
+        fake_date = fake_gen.date()
+        fake_name = fake_gen.company()
+
+        # Create the new data to enter db
+        web_page = Webpage.objects.get_or_create(topic=top, url=fake_url, name=fake_name)[0]
+
+        # Create fake log access
+        record = AccessRecord.objects.get_or_create(name=web_page, date=fake_date)[0]
