e16c9136fed3778a2ce3840efcf6a0e60c826f09
Danilo Cunha <danilokage@gmail.com>
2020-03-18 09:34:48 -0300

    [basicforms project] Add usage of django core validators, and check input w/ custom validators

--- basicforms/basicapp/forms.py
+++ basicforms/basicapp/forms.py
@@ -1,19 +1,26 @@
-from tkinter.tix import Form
 from django import forms
+from django.core import validators
+
+
+def check_for_z(value):
+    if value[0].lower() != 'z':
+        raise forms.ValidationError("Name needs to start with letter 'Z'.")
 
 
 class FormUser (forms.Form):
-    name = forms.CharField()
+    name = forms.CharField(validators=[check_for_z])
     email = forms.EmailField()  # # Email field requires a @, and a ".something" suffix to pass is_valid() function
+    email_verify = forms.EmailField(label='Enter your e-mail again')
     text = forms.CharField(widget=forms.Textarea)
 
-
     # Security variable
-    bot_catcher = forms.CharField(required=False, widget=forms.HiddenInput)
+    bot_catcher = forms.CharField(required=False, widget=forms.HiddenInput,
+                                  validators=[validators.MaxLengthValidator(0)])
 
-    def clean_bot_catcher(self):
-        bot_catcher = self.cleaned_data['bot_catcher']
+    def clean(self):
+        all_clean_data = super(FormUser, self).clean()
+        email = all_clean_data['email']
+        email_v = all_clean_data['email_verify']
 
-        if len(bot_catcher) > 0:
-            raise forms.ValidationError("Bot behavior detected.")
-
+        if email != email_v:
+            raise forms.ValidationError("E-mails doesn't match!")
