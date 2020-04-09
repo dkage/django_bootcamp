1b3d62c023148ece29b082a35b1e2a04be0b74ad
Danilo Cunha <danilokage@gmail.com>
2020-04-08 22:01:56 -0300

    [blog] Add models, add app to settings

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
+from django.utils.timezone import now
+import django.utils.timezone as tz
 
-# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.DO_NOTHING)
+    create_date = models.DateTimeField(default=tz.now)
+    published_date = models.DateTimeField(blank=True, null=True)
+
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+
+    def publish(self):
+        self.published_date = tz.now()
+        self.save()
+
+    def approve_comments(self):
+        return self.comments.filter(approved_comments=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    author = models.CharField(max_length=200)
+    post = models.ForeignKey('blog.Post', on_delete=models.DO_NOTHING)
+    text = models.TextField()
+    create_date = models.DateTimeField(default=tz.now)
+
+    approved_comment = models.BooleanField(default=False)
