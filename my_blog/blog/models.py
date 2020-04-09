94b83b6f11f630c15c3042b65804d72b3a36387e
Danilo Cunha <danilokage@gmail.com>
2020-04-09 12:14:10 -0300

    [blog] Add more defs to models, add related_name to comment

--- my_blog/blog/models.py
+++ my_blog/blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.utils.timezone import now
 import django.utils.timezone as tz
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -18,14 +18,27 @@ class Post(models.Model):
     def approve_comments(self):
         return self.comments.filter(approved_comments=True)
 
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk': self.pk})
+
     def __str__(self):
         return self.title
 
 
 class Comment(models.Model):
     author = models.CharField(max_length=200)
-    post = models.ForeignKey('blog.Post', on_delete=models.DO_NOTHING)
+    post = models.ForeignKey('blog.Post', related_name='comments', on_delete=models.DO_NOTHING)
     text = models.TextField()
     create_date = models.DateTimeField(default=tz.now)
 
     approved_comment = models.BooleanField(default=False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
