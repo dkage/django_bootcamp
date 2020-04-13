b8ff918565fbf88e9a5fb9357945431abbc1ead9
Danilo Cunha <danilokage@gmail.com>
2020-04-13 13:54:09 -0300

    [blog] Add urls, add detail view for post model

--- my_blog/blog/urls.py
+++ my_blog/blog/urls.py
@@ -3,5 +3,8 @@ from blog.views import *
 
 
 urlpatterns = [
-    path('about', AboutView.as_view(), name='about')
+    path('', PostListView.as_view(), name='post_list'),
+    path('about', AboutView.as_view(), name='about'),
+    path('post/<slug:slug>', DetailView.as_view(), name='post_detail'),
+
 ]
--- my_blog/blog/views.py
+++ my_blog/blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 import django.utils.timezone as tz
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView
 from blog.models import *
 
 
@@ -13,4 +13,9 @@ class PostListView(ListView):
     model = Post
 
     def get_queryset(self):
-        return Post.objects.filter(published_date__lte=tz.now.order_by('-published_date'))
+        return Post.objects.filter(published_date__lte=tz.now().order_by('-published_date'))
+
+
+class PostDetailView(DetailView):
+    model = Post
+
