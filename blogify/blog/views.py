22dfcb77d7c186df887e21009ad164a815505035
Danilo Cunha <danilokage@gmail.com>
2020-06-24 23:58:09 -0300

    Add new templates, add view function for commenting on post

--- blogify/blog/views.py
+++ blogify/blog/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
 from blog.forms import PostForm, CommentForm
 from blog.models import Post, Comment
 from django.utils import timezone
@@ -47,3 +48,20 @@ class DraftListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return Post.objects.filter(published_date__isnull=True).order_by('-created_date')
+
+
+########################
+# Comments Related Views
+@login_required
+def add_comment_to_post(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', pk=post.pk)
+    else:
+        form = CommentForm()
+    return render(request, 'blog/comment_form.html', {'form': form})
