ad4c8b22a360a2f8015ca7b64c1d0253a523013d
Danilo Cunha <danilokage@gmail.com>
2020-06-25 18:16:00 -0300

    Add new routes and view functions

--- blogify/blog/urls.py
+++ blogify/blog/urls.py
@@ -11,4 +11,7 @@ urlpatterns = [
     path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
     path('post/<int:pk>/remove/', views.PostDeleteView.as_view(), name='post_delete'),
     path('drafts/', views.DraftListView.as_view(), name='post_draft_list'),
+    path('post/<int:pk>/comment/', views.add_comment_to_post, name='add_comment_to_post'),
+    path('comment/<int:pk>/approve', views.comment_approve, name='comment_approve'),
+    path('post/<int:pk>/publish', views.post_publish, name='post_publish')
 ]
--- blogify/blog/views.py
+++ blogify/blog/views.py
@@ -65,3 +65,25 @@ def add_comment_to_post(request, pk):
     else:
         form = CommentForm()
     return render(request, 'blog/comment_form.html', {'form': form})
+
+
+@login_required
+def post_publish(request, pk):
+    post = get_object_or_404(pk)
+    post.publish()
+    return redirect('post_detail', pk=pk)
+
+
+@login_required
+def comment_approve(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    comment.approve()
+    return redirect('post_detail', pk=comment.pk)
+
+
+@login_required
+def comment_remove(request, pk):
+    comment = get_object_or_404(Comment, pk=pk)
+    post_pk = comment.post.pk
+    comment.delete()
+    return redirect('post_detail', pk=post_pk)
